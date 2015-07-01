e5f53822bcf88e49fb012546f1ebca2a530c4057
MKS-students <mks.students@hackreactor.com>
2015-07-01 16:38:30 -0700

    data structures sprint 1 passes all tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,30 @@ var Stack = function(){
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {},
+      count = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function(){
+    var result;
+
+    if (count < 1) {
+      return;
+    }
+    count--;
+    result = storage[count];
+    delete storage[count];
+
+    return result;
   };
 
   someInstance.size = function(){
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var result = Object.create(stackMethods);
+  result.count = 0;
+  result.storage = {};
+
+  return result;
 };
 
 var stackMethods = {};
 
+stackMethods.pop = function() {
+  var result;
+  if ( this.count < 1 ) {
+    return;
+  }
+  this.count--;
+  result = this.storage[this.count];
+  delete this.storage[this.count];
 
+  return result;
+};
+
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+stackMethods.size = function() {
+  return this.count;
+};
