a25722de9ad90aebb0b6add27fe653d6338ff81f
MKS-students <mks.students@hackreactor.com>
2015-07-02 12:34:52 -0700

    add tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,28 +1,45 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
+  newTree.children = [];
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree,treeMethods);
 
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  // create new tree node with value
+  var newTree = Tree(value);
 
+  // push to tree instance
+  this.children.push(newTree);
 };
 
 treeMethods.contains = function(target){
+  // function that checknode's value
+  var checkNodeValue = function (node) {
+    // if the node's value === target
+    if (node.value === target) {
+      return true;
+    }
 
+    // else if it has children
+    if (node.children) {
+      // for each of those children, we'll pass each node into the checknode function
+      for (var i = 0; i < node.children.length; i++) {
+        // if any of these return true
+        if( checkNodeValue(node.children[i]) ) {
+          return true;
+          // return true
+        }
+      }
+    }
+
+    return false;
+  };
+
+  return checkNodeValue(this);
 };
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
