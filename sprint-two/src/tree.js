0683a5b2ba399878eb540d838f902e96126f7e0f
Quinton Aiken <quintonlaiken@gmail.com>
2015-07-01 23:33:35 -0700

    complete tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,22 +3,37 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+
+  _.extend(newTree,treeMethods);
 
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
+
+  var searchNodes = function(node) {
+    if( node.value === target ) {
+      return true;
+    }
+    if( node.children ) {
+      for(var i=0; i < node.children.length; ++i) {
+        if( searchNodes(node.children[i]) ) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  };
+
+  return searchNodes(this);
 
 };
 
