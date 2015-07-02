ef622cb145ef4162056278a9570a1d73597899ac
MKS-students <mks.students@hackreactor.com>
2015-07-01 18:28:59 -0700

    linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,43 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    // create Node(value)
+    var node = Node(value);
+
+    // if this.head === null set this.head = Node(value)
+    if (!this.head) {
+      this.head = node;
+    }
+
+    // if this.tail exists, set this.tail's next to Node(value)
+    if (this.tail) {
+      this.tail.next = node;
+    }
+
+    // set list's tail to Node(value)
+    this.tail = node;
   };
 
   list.removeHead = function(){
+    // if no head, return
+    if (!this.head) {
+      return;
+    }
+    // else cache the head's next
+    var currHead = this.head;
+    var nextHead = currHead.next;
+
+    // set head to cached node
+    this.head = nextHead;
+
+    return currHead.value;
   };
 
   list.contains = function(target){
+    // checkNodeValue - recursive, accepts node
+    //   check if node.value === target, return true
+    //   if node.next !== null, call it again
+    //   else return false
   };
 
   return list;
