af21244d67235e52691fd20a6d906b0e454ca1f7
Quinton Aiken <quintonlaiken@gmail.com>
2015-07-02 22:55:43 -0700

    quinton solution to binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,78 @@
 var BinarySearchTree = function(value){
+  var result = Object.create(BinarySearchTree.prototype);
+  result.value = value;
+  result.left = null;
+  result.right = null;
+  return result;
 };
+
+BinarySearchTree.prototype.insert = function(value) {
+
+  var setNodeValue = function(node,value) {
+
+    if( value < node.value ) {
+      if(!node.left) {
+        node.left = BinarySearchTree(value);
+      } else {
+        setNodeValue(node.left, value);
+      }
+    }
+
+    if( value > node.value ) {
+      if(!node.right) {
+        node.right = BinarySearchTree(value);
+      } else {
+        setNodeValue(node.right, value);
+      }
+    }
+
+  };
+
+  setNodeValue(this,value);
+
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+
+  var checkTreeforValues = function(node,value) {
+
+    if( node.value === value ) {
+      return true;
+    }
+
+    if( node.left && checkTreeforValues(node.left, value) ) {
+      return true;
+    }
+
+    if( node.right && checkTreeforValues(node.right, value) ) {
+      return true;
+    }
+
+    return false;
+
+  };
+
+  return checkTreeforValues(this,value);
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+
+  var runCb = function(node,value) {
+    cb(node.value);
+
+    if( node.left ) {
+      runCb(node.left);
+    }
+
+    if( node.right ) {
+      runCb(node.right);
+    }
+  };
+
+  runCb(this,cb);
+};
+
 
 
 /*
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,20 +4,28 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+  var i = this.getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i,v);
 };
 
 HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var i = this.getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(i);
 };
 
 HashTable.prototype.remove = function(k){
-
+  this.insert(k,null);
 };
 
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+// This is a "hashing function". You don't need to worry about it, just use it
+// to turn any string into an integer that is well-distributed between the
+// numbers 0 and `max`
+HashTable.prototype.getIndexBelowMaxForKey = function(str, max){
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = (hash<<5) + hash + str.charCodeAt(i);
+    hash = hash & hash; // Convert to 32bit integer
+    hash = Math.abs(hash);
+  }
+  return hash % max;
+};
