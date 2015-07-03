b886ed52ec8ef06c4cf2e17f15cdc80ef4ad669f
MKS-students <mks.students@hackreactor.com>
2015-07-02 21:21:34 -0700

    add binarySearchTree work

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,37 @@
 var BinarySearchTree = function(value){
+  var result = Object.create(BinarySearchTree.prototype);
+
+  result.value = value;
+  result.left = null;
+  result.right = null;
+
+  return result;
+};
+
+BinarySearchTree.prototype.insert = function (newValue) {
+  var assignNextNode = function (node, value) {
+    if ( value < node.value && !node.left ) {
+      return node.left = BinarySearchTree(value);
+    } else {
+      assignNextNode(node.left,value);
+    }
+
+    if ( value > node.value && !node.right ) {
+      return node.right = BinarySearchTree(value);
+    } else {
+      assignNextNode(node.right,value);
+    }
+  };
+
+  assignNextNode(this, newValue);
+};
+
+BinarySearchTree.prototype.contains = function () {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function () {
+
 };
 
 
